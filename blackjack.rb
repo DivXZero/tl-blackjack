c96d6eb4bb479b41544b191145c530a7e0ca1f77
DivXZero <silv3rlogic@gmail.com>
2014-08-27 05:02:35 -0700

    Finished Game

--- blackjack.rb
+++ blackjack.rb
@@ -46,9 +46,7 @@ def get_card_total(player)
   player[:cards].each do |card|
     x = card[0]
     if x == 'A'
-      if total > 21
-        total -= 10
-      end
+      total -= 10 if total > 21
     end
   end
 
@@ -82,45 +80,109 @@ def check_for_outcome(player, dealer)
     puts 'Dealer has blackjack :('
   elsif player_total > 21
     puts 'Bust :('
-  elsif player_total == 21
-    puts 'Blackjack!'
-  elsif dealer_total > 21
-    puts 'Dealer busts :)'
-  elsif player_total < 21 && dealer_total < player_total
-    puts 'Player wins :) (high card)'
-  elsif dealer_total > player_total
-    puts 'Dealer wins :( (high card)'
   elsif dealer_total == player_total
     puts 'Game is a draw :|'
-end
+    payout(player, 1)
+  elsif player_total == 21
+    puts 'Blackjack!'
+    payout(player, 3)
+  elsif dealer_total > 21
+    puts 'Dealer busts :)'
+    payout(player, 2)
+  elsif player_total < 21 && dealer_total < player_total
+    puts 'Player wins :) (high card)'
+    payout(player, 2)
+  elsif dealer_total > player_total
+    puts 'Dealer wins :( (high card)'
+  end
+  puts "\n~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~\n\n"
 end
 
 def display_cards(player)
-  puts "#{player[:cards].inspect} Total: #{get_card_total(player)}"
+  top_row = ''
+  middle_row = ''
+  middle2_row = ''
+  middle3_row = ''
+  bottom_row = ''
+
+  player[:cards].each do |card|
+    spacer = ' '
+    spacer = '' if card[0].length == 2
+    top_row += "/-----\\"
+    middle_row += "|#{card[0]} #{spacer} #{card[1]}|"
+    middle2_row += "|     |"
+    middle3_row += "|#{card[1]} #{spacer} #{card[0]}|"
+    bottom_row += "\\-----/"
+  end
+
+  if player.has_key?(:name)
+    puts player[:name] + "\t\tCash: $#{player[:money]}\t\tBet: $#{player[:bet]}"
+  else
+    puts 'Dealer'
+  end
+  puts top_row
+  puts middle_row
+  puts middle2_row + "\tTotal: #{get_card_total(player)}"
+  puts middle3_row
+  puts bottom_row
+  puts ''
+end
+
+def clear_screen
+  system('cls')
+  system('clear')
+  puts "\n~*~*~*~*~*~*~*~*~ Blackjack ~*~*~*~*~*~*~*~*~\n\n"
+end
+
+def payout(player, multiplier)
+  amount = player[:bet] * multiplier
+  player[:money] += amount
+  puts "Payout: $#{amount} (#{multiplier}x)"
+end
+
+def run_game(player, dealer, deck)
+  clear_screen
+  player[:money] = get_input("Hello, #{player[:name]}! How much would you like to play with today? (e.g. 500)").to_f until player[:money] > 0
+  clear_screen
+
+  while player[:money] > 0 do
+    deck = init_deck
+    player[:cards] = []
+    dealer[:cards] = []
+    player[:bet] = 0
+    player[:bet] = get_input("How much would you like to bet this hand? [Cash: $#{player[:money]}]").to_f until player[:bet] > 0 && player[:bet] <= player[:money]
+    player[:money] -= player[:bet]
+    clear_screen
+
+    # Initial Deal
+    2.times { deal_card(player, deck); deal_card(dealer, deck) }
+
+    display_cards(dealer)
+    display_cards(player)
+
+    process_player(player, deck)
+    process_dealer(dealer, deck)
+
+    clear_screen
+    check_for_outcome(player, dealer)
+    display_cards(dealer)
+    display_cards(player)
+  end
+
+  options = { 'y' => true, 'n' => false }
+  play_again = nil
+  play_again = options[get_input('Play Again? (y/n)').downcase] until play_again != nil
+  run_game(player, dealer, deck) if play_again
 end
 
 SUITS = ['H', 'D', 'C', 'S']
 VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
-deck = init_deck
+deck = []
 player = { name: '', money: 0.0, bet: 0.0, cards: [] }
 dealer = { cards: [] }
 
-#player[:name] = get_input("What's your name?")
-#player[:money] = get_input("Hello, #{player[:name]}! How much would you like to play with today? (e.g. 500)").to_f until player[:money] > 0
-2.times { deal_card(player, deck); deal_card(dealer, deck) }
-game_over = false
-while !game_over do
-  puts 'Player:'
-  display_cards(player)
-  puts 'Dealer:'
-  display_cards(dealer)
-  process_player(player, deck)
-  process_dealer(dealer, deck)
-  puts 'Player:'
-  display_cards(player)
-  puts 'Dealer:'
-  display_cards(dealer)
-  check_for_outcome(player, dealer)
-  game_over = true
-end
+clear_screen
+player[:name] = get_input("What's your name?")
+
+run_game(player, dealer, deck)
